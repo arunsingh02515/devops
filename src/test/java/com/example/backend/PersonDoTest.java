6783197aad372c44bc1c852c657a3a90e93bb1a4
Arun Kumar <arun.kumar@nwea.org>
2020-04-25 14:01:01 +0530

    add test case

---
+++ src/test/java/com/example/backend/PersonDoTest.java
@@ -0,0 +1,21 @@
+package com.example.backend;
+
+
+import org.junit.jupiter.api.Test;
+
+public class PersonDoTest {
+
+
+
+
+@Test
+
+public void test(){
+
+PersonDo personDo= new PersonDo();
+
+personDo.setName("arun");
+personDo.setAddress("pp");
+
+}
+}
